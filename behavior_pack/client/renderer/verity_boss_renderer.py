37d80872da60780d648e3d7ad61f05099a3a9ee1
zsj20130818 <zsj201308182026@163.com>
2026-09-25 20:08:04 +0800

    Delete behavior_pack directory

--- behavior_pack/client/renderer/verity_boss_renderer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""VerityBossRenderer - 网易基岩版"""
-
-from __future__ import print_function, division, absolute_import, unicode_literals
-
-
-class VerityBossRenderer(object):
-    """Boss 渲染器 —— 网易基岩版通过 resource_pack/entity/ 定义。"""
-    pass
