08face42485f1e6cb7a219e53ca65899897dd5eb
zsj20130818 <zsj201308182026@163.com>
2026-09-25 21:43:27 +0800

    Delete behavior_pack directory

--- behavior_pack/client/renderer/hallucination_renderer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""HallucinationRenderer - 网易基岩版"""
-
-from __future__ import print_function, division, absolute_import, unicode_literals
-
-
-class HallucinationRenderer(object):
-    """幻觉渲染器 —— 网易基岩版通过 resource_pack/entity/ 定义。"""
-    pass
--- behavior_pack/verity.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Verity 主类 - 网易基岩版"""
-
-from __future__ import print_function, division, absolute_import, unicode_literals
-
-MOD_ID = "verity"
